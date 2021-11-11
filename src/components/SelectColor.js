ab75702e864287e25806b7a9845ce05197773651
DL <neoxrock@gmail.com>
2021-11-11 13:20:38 +1100

    Select colors

---
+++ src/components/SelectColor.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const colors = ["#FFC0CB", "#FFB6C1", "#FF69B4", "#FF1493", "#C71585", "#DB7093"];
+
+const SelectColor = () => {
+  return (
+    <div>
+      <h2>Choose a color</h2>
+      <div style={{ display: "flex" }}>
+        {colors.forEach((color) => {
+          <div
+            key={color}
+            style={{
+              background: 'color',
+              width: "24px",
+              height: "24px",
+              cursor: "pointer",
+            }}
+          >Test</div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default SelectColor;
