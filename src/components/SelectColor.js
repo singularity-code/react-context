305ba0cfe473b80bd1d4029b64b7fba74b636d28
DL <neoxrock@gmail.com>
2021-11-11 13:32:16 +1100

    Change colors

--- src/components/SelectColor.js
+++ src/components/SelectColor.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ColorContext from "../contexts/color";
 
 const colors = [
   "#FFC0CB",
@@ -13,19 +14,29 @@ const SelectColor = () => {
   return (
     <div>
       <h2>Choose a color</h2>
-      <div style={{ display: "flex" }}>
-        {colors.map((color) => (
-          <div
-            key={color}
-            style={{
-              background: color,
-              width: "24px",
-              height: "24px",
-              cursor: "pointer",
-            }}
-          ></div>
-        ))}
-      </div>
+      <ColorContext.Consumer>
+        {({ actions }) => (
+          <div style={{ display: "flex" }}>
+            {colors.map((color) => (
+              <div
+                key={color}
+                style={{
+                  background: color,
+                  width: "24px",
+                  height: "24px",
+                  cursor: "pointer",
+                }}
+                onClick={() => actions.setColor(color)}
+                // right click to change subcolor
+                onContextMenu={(e) => {
+                  e.preventDefault();
+                  actions.setSubcolor(color);
+                }}
+              ></div>
+            ))}
+          </div>
+        )}
+      </ColorContext.Consumer>
     </div>
   );
 };
